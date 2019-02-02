3520f795b2c75c2f1bda590ff43157be162703f8
Ryan Billings <rbillings@customink.com>
2019-02-02 08:22:21 -0500

    Add linter, validations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-export default function(){
-  return 42;
+import zipcodes from 'zipcodes'
+
+export class InvalidZipError extends Error {}
+
+const validateZip = zipCode => /^\d{5}(-\d{4})?$/.test(zipCode)
+
+export default function (zipCode) {
+  if (!validateZip(zipCode)) {
+    throw new InvalidZipError('Zipcode must be valid')
+  }
+
+  return zipcodes.random().zip
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,13 @@
-import zipToDma from '../src/index.js';
+import zipToDma, { InvalidZipError } from '../src/index.js'
+
+describe('validations', () => {
+  test('zipcodes must be 5 digits', () => {
+    expect(() => zipToDma('abc')).toThrow(InvalidZipError)
+    expect(() => zipToDma(123456)).toThrow(InvalidZipError)
+    expect(() => zipToDma()).toThrow(InvalidZipError)
+  })
+})
 
 test('maps zip codes to DMA codes', () => {
-  expect(zipToDma('22206')).toBe(42);
-});
+  expect(zipToDma('22206')).toBe(42)
+})
